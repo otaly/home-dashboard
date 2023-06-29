7dfee1d54a4c70e3e0e99710df5e304c636bbc20
otaly <60144056+otaly@users.noreply.github.com>
2023-06-29 10:37:01 +0900

    SubscriptionBadgeをコンポーネントとして分割

---
+++ app/_components/Cards/Subscriptions/Subscription.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import type { SubscriptionCategory } from '@/app/_types';
+import { formatMoney } from '@/utils/format';
+import { Card, Flex, Metric, Text } from '@tremor/react';
+import { SubscriptionBadge } from './SubscriptionBadge';
+
+type SubscriptionProps = {
+  serviceName: string;
+  price: number;
+  category: SubscriptionCategory;
+};
+
+export const Subscription = ({
+  serviceName,
+  price,
+  category,
+}: SubscriptionProps) => (
+  <Card>
+    <Flex justifyContent="start" alignItems="stretch" flexDirection="col">
+      <Text>{serviceName}</Text>
+      <Metric>
+        {formatMoney(price)} / <span className="text-[1.65rem]">月</span>
+      </Metric>
+      <div className="mt-4">
+        <SubscriptionBadge category={category} />
+      </div>
+    </Flex>
+  </Card>
+);
---
+++ app/_components/Cards/Subscriptions/SubscriptionBadge.tsx
@@ -0,0 +1,36 @@
+import { SubscriptionCategory } from '@/app/_types';
+import {
+  BoltIcon,
+  FilmIcon,
+  MusicalNoteIcon,
+} from '@heroicons/react/24/outline';
+import { Badge } from '@tremor/react';
+import { ElementType } from 'react';
+
+type BadgeMapping = {
+  [key in SubscriptionCategory]: { icon: ElementType; text: string };
+};
+
+const badgeMap: BadgeMapping = {
+  video: {
+    icon: FilmIcon,
+    text: 'video',
+  },
+  music: {
+    icon: MusicalNoteIcon,
+    text: 'music',
+  },
+  tool: {
+    icon: BoltIcon,
+    text: 'tool',
+  },
+};
+
+type SubscriptionBadgeProps = {
+  category: SubscriptionCategory;
+};
+
+export const SubscriptionBadge = ({ category }: SubscriptionBadgeProps) => {
+  const badge = badgeMap[category];
+  return <Badge icon={badge.icon}>{badge.text}</Badge>;
+};
