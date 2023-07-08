715ee40d2a6a66a9d67b40553671909e95ca75d4
naoki ota <otaly>
2023-07-08 14:39:13 +0900

    ただTSの型を勉強しただけのコミット

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,3 +1,36 @@
-const Page = () => <p className="p-20 text-center">users</p>;
+/*
+  { success: boolean; error?: string }
+  ↑だと if(!success) ブロックの中でもerrorは string | undefined
+*/
+type Result = { success: true } | { success: false; error: string };
+
+/*
+  if('data' in resultWithData)ブロックの中ではResultWithDataの型が{ data: { id: string, name: string } }に確定する。
+  { data?: {...}, error?: {...} }という型にするより良い(ただ、少なくともVSCodeでは「resultWithData.」まで打った時にプロパティ名の補完が効かなくなる...)。
+*/
+type ResultWithData =
+  | { data: { id: string; name: string } }
+  | { error: string };
+
+const fetch: () => Promise<Result> = () =>
+  Promise.resolve({ success: false, error: 'sample error' });
+const fetchData: () => Promise<ResultWithData> = () =>
+  Promise.resolve({ data: { id: 'id', name: 'name' } });
+
+const Page = async () => {
+  const result = await fetch();
+  if (!result.success) {
+    const error = result.error;
+    console.error(error);
+  }
+
+  const resultWithData = await fetchData();
+  if ('data' in resultWithData) {
+    const { id, name } = resultWithData.data;
+    console.log(id, name);
+  }
+
+  return <p className="p-20 text-center">users</p>;
+};
 
 export default Page;
